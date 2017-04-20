ea282146fde9a9b05cf84e167a18af88c8e74486
Mina Samy <minasamy_@hotmail.com>
2017-04-20 13:29:03 +0200

    Adding and moving some files
    Added:
    - Menus folder
    - Main menu 
    Moved:
    - Entry menu to Menus folder

---
+++ CCL Oil Labs Control/Menus/EntryMenu.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CCL_Oil_Labs_Control
+{
+    /// <summary>
+    /// Interaction logic for EntryMenu.xaml
+    /// </summary>
+    public partial class EntryMenu : Window
+    {
+        public EntryMenu()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Switch to MainMenu when EntryMenuButtonMainMenu is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EntryMenuButtonMainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            new CCL_Oil_Labs_Control.Menus.MainMenu().Show();
+        }
+
+        /// <summary>
+        /// Closes program when EntryMenuButtonClose is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EntryMenuButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ CCL Oil Labs Control/Menus/MainMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CCL_Oil_Labs_Control.Menus
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MainMenuButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
