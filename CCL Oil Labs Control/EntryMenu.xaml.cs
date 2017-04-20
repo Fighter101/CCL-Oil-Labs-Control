c2ddd176e204a3fe84b019e735e732207002c130
Mina Samy <minasamy_@hotmail.com>
2017-04-20 03:53:00 +0200

    Added an Entry Menu
    A starting point in GUI

---
+++ CCL Oil Labs Control/EntryMenu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CCL_Oil_Labs_Control
+{
+    /// <summary>
+    /// Interaction logic for EntryMenu.xaml
+    /// </summary>
+    public partial class EntryMenu : Window
+    {
+        public EntryMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
