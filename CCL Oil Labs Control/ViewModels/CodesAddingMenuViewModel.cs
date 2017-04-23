0662f81d97fdf62b0140aa2d92595648217b7ef1
Hassan Saied <hsaied117@yahoo.com>
2017-04-23 23:27:10 +0200

    Really no change, just need to merge with master

--- CCL Oil Labs Control/ViewModels/CodesAddingMenuViewModel.cs
+++ CCL Oil Labs Control/ViewModels/CodesAddingMenuViewModel.cs
@@ -5,11 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Prism.Mvvm;
 using Prism.Commands;
-
+using CCL_Oil_Labs_Control.CompositeCommands;
 namespace CCL_Oil_Labs_Control.ViewModels
 {
     public class CodesAddingMenuViewModel : BindableBase
     {
 
+        
+        public CodesAddingMenuViewModel(GlobalNavigateCommand globalNavigateCommand)
+        {
+
+        }
     }
 }
