3a58cb461b9da92717454dad0614aec1c034b396
Mina Samy <minasamy_@hotmail.com>
2017-04-24 00:32:03 +0200

    Version 1.1 GUI

--- CCL Oil Labs Control/Utils/Utils.cs
+++ CCL Oil Labs Control/Utils/Utils.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Security;
 namespace CCL_Oil_Labs_Control.Utils
